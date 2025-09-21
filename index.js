df1b8725813de8ef1cfa0d03af734099b0a6e0af
brianwaithaka <waithakabrian8@gmail.com>
2025-09-21 16:00:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  //   debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    // debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+// 1. assessment
+
+function writeCards(nameArray, string) {
+  let messages = [];
+  for (let index = 0; index < nameArray.length; index++) {
+    messages.push(
+      `Thank you, ${nameArray[index]}, for the wonderful ${string} gift!`
+    );
+  }
+  console.log(messages);
+
+  return messages;
+}
+
+// ("Thank you, Guadalupe, for the wonderful surprise gift!");
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+  let index = number;
+  while (index >= 0) {
+    console.log(index);
+    index--;
+  }
+}
+countDown(10);
